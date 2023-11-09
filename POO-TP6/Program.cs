aa3d7e1169508c8bac761713de1bed1aabc699cf
gabrieltdrk <gabriel.andradesx@gmail.com>
2023-11-08 21:50:29 -0300

    tudo

---
+++ POO-TP6/Program.cs
@@ -0,0 +1,76 @@
+﻿class Product
+{
+    public float price { get; set; } = 0;
+    public bool isImported { get; set; }
+    public virtual void Get_Price()
+    {
+        Console.WriteLine(price);
+    }
+}
+
+class Book : Product
+{
+    public float paper_tax { get; set; } = 0;
+    public override void Get_Price()
+    {
+        Console.WriteLine(price + paper_tax);
+    }
+}
+
+class Eletronics : Product
+{
+    public float repair_cost { get; set; } = 0;
+    public float maintenance { get; set; } = 0;
+    public float firmware_price { get; set; } = 0;
+    public override void Get_Price()
+    {
+        Console.WriteLine(price + repair_cost + maintenance + firmware_price);
+    }
+}
+
+class Perfume : Product
+{
+    public float essence_tax { get; set; } = 0;
+    public float bottle_tax { get; set; } = 0;
+    public override void Get_Price()
+    {
+        Console.WriteLine(price + essence_tax + bottle_tax);
+    }
+}
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // PRODUTO
+        Product product = new Product();
+        product.price = 250;
+        product.isImported = true;
+        product.Get_Price();
+
+        // LIVRO
+        Book book = new Book();
+        book.price = 25;
+        book.isImported = false;
+        book.paper_tax = 10;
+        book.Get_Price();
+
+        //ELETRONICOS
+        Eletronics eletronics = new Eletronics();
+        eletronics.price = 3500;
+        eletronics.isImported = true;
+        eletronics.firmware_price = 10000;
+        eletronics.repair_cost = 200;
+        eletronics.maintenance = 895;
+        eletronics.Get_Price();
+
+        //PERFUME
+        Perfume perfume = new Perfume();
+        perfume.price = 650;
+        perfume.isImported = false;
+        perfume.essence_tax = 35;
+        perfume.bottle_tax = 150;
+        perfume.Get_Price();
+    }
+}
---
+++ README.md
@@ -0,0 +1,17 @@
+<h1 align="center"> 🧑‍💻 Programação Orientada a Objetos <br> Trabalho Prático #5</h1>
+
+<img align="center" src="https://fenestra.com.br/wp-content/uploads/2021/03/POO.jpg">
+
+## 🔍 Aula 6 - Polimorfismo
+
+1a) Polimorfismo de sobrescrita (ou polimorfismo de método): Nesse tipo de polimorfismo, uma 
+subclasse (classe filha) pode fornecer uma implementação específica de um método que é 
+definido em sua classe ancestral (classe pai). Quando você chama o método em um objeto 
+da subclasse, a versão sobrescrita desse método na subclasse é executada. Isso permite 
+que diferentes classes forneçam implementações específicas de um mesmo método.
+
+### ⚒️ Exercícios da Aula:
+
+1- Suponha que você esteja construindo um sistema para uma loja online e precisa lidar com diferentes tipos de produtos, como livros, eletrônicos e roupas. Cada tipo de produto tem seu próprio método de cálculo de preço de envio.
+
+<div align="center">Coded by <a href="https://github.com/gabrieltdrk">gabrieltdrk</a>.</div>
